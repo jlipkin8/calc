5a8c49a7c392185cbb9b49414173e8a39377ee48
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-06 11:49:36 -0700

    subtraction fixed again and multiplication fixed

--- arithmetic.py
+++ arithmetic.py
@@ -8,11 +8,12 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-    return num2 - num1
+    return num1 - num2
 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
+    return num1 * num2
 
 
 def divide(num1, num2):
